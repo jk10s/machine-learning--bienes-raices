0d742faf3d6e0ce37bc60bf85099a17c69b4002f
jk10s <juanka_267@hotmail.com>
2022-07-15 22:37:50 -0500

    prediciones

--- exampleone/celcios.py
+++ exampleone/celcios.py
@@ -13,7 +13,7 @@ teperatuta_df = pd.read_csv("celsius_a_fahrenheit.csv")
 
 sns.scatterplot(teperatuta_df['Celsius'],teperatuta_df['Fahrenheit'])
 #cargando datos
-x_train= teperatuta_df['Celsius']
+X_train= teperatuta_df['Celsius']
 y_train= teperatuta_df['Fahrenheit']
 
 #creando el modelo
@@ -21,4 +21,18 @@ model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(units=1, input_shape=[1]))
 
 #model.summary()
-model.compile(optimizer=tf.keras.optimizers.Adam(0.5), loss='')
+model.compile(optimizer=tf.keras.optimizers.Adam(0.5), loss='mean_squared_error')
+#entrenando el modelo
+epochs_hist = model.fit(X_train, y_train, epochs = 100)
+
+#Evaluando el modelo entrenado
+epochs_hist.history.keys()
+
+plt.plot(epochs_hist.history['loss'])
+plt.title('Progreso de Pérdida durante Entrenamiento del Modelo')
+plt.xlabel('Epoch')
+plt.ylabel('Training Loss')
+plt.legend('Training Loss')
+plt.show()
+
+#prediciones
